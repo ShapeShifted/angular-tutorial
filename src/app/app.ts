2daf8c75e05460dcfaa814a4d3076d8a57ee6f00
William Yee <qing530@hotmail.com>
2025-08-27 15:16:45 +0800

    Add components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login, ProfileComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = 'Tom Cruise';
+  handleClickEvent() {
+    alert('Button clicked!');
+  }
 }
