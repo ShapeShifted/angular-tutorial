b00564bdf3c24d5974f3fc1fedfeab841452a2a3
William Yee <qing530@hotmail.com>
2025-08-28 12:14:24 +0800

    Add Style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Home } from './homepage/homepage';
 import { Getset } from './getset/getset';
+import { Style } from './style/style';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/page1', pathMatch: 'full' },
     { path: 'page1', component: Home },
     { path: 'page2', component: Counter },
-    { path: 'page3', component: Getset }
+    { path: 'page3', component: Getset },
+    { path: 'page4', component: Style },
 ];
 
 @NgModule({
