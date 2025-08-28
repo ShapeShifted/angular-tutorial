46c60037206677b111eb27281cf407f25ce824c5
William Yee <qing530@hotmail.com>
2025-08-28 16:07:16 +0800

    add switch

--- src/app/switch/switch.ts
+++ src/app/switch/switch.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './switch.scss'
 })
 export class Switch {
+  color = 'blue';
+  handleColour(val:string)
+  {
+    this.color = val;
+  }
 
+  chooseColor(event:Event)
+  {
+    this.color = (event.target as HTMLInputElement).value;
+  }
 }
